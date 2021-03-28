2b567e15003dc404d70384143cc7de626e3c40e9
todoni <hansojeong95@gmail.com>
2021-03-28 16:18:15 +0900

    ex01(delete \n) ex07(Change static component of sed -n *,*p to env variable) Modified - 2nd submit

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat /etc/passwd | grep -v '^#' | awk -F: '{print $1}' | awk 'NR%2==0' | rev | sort -r | sed -n 1,5p | sed s'/$/, /' |tr -d '\n' | sed 's/, $/./' | tr -d '\n'
+cat /etc/passwd | grep -v '^#' | awk -F: '{print $1}' | awk 'NR%2==0' | rev | sort -r | sed -n ${FT_LINE1},${FT_LINE2}p | sed s'/$/, /' |tr -d '\n' | sed 's/, $/./' | tr -d '\n'
